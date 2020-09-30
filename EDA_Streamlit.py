f202161d084fbc849488f2a66113983d669ed4de
iryan9401 <iryan9401@gmail.com>
2020-09-30 18:39:52 -0400

    Add existing file

--- EDA_Streamlit.py
+++ EDA_Streamlit.py
@@ -9,32 +9,97 @@ Created on Mon Sep 28 13:27:04 2020
 import streamlit as st
 import pandas as pd
 import numpy as np
+import io
+import seaborn as sns
+import matplotlib.pyplot as plt
 
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-
+st.set_option('deprecation.showPyplotGlobalUse', False)
 
 def main():
   st.title('Welcome to the EDA web application!')
   file = st.file_uploader("Upload .csv file for analysis", type = ['csv'])
   if file:
-    data = pd.read_csv(file)
-    st.write('Data Summary \n')
+    csv = pd.read_csv(file)
+    data = pd.DataFrame(csv)
+    st.write('Data Successfully Loaded.  \n')
+    st.write('Dimensions:')
+    st.write(data.shape)
+    st.write('Sample data:')
     st.write(data.head())
+    columns = data.columns.to_list()
+    
+    
+    # if st.checkbox('Check Info'):
+    #     buf = io.StringIO()
+    #     data.info(verbose = True,buf=buf)
+    #     s = buf.getvalue()
+    #     st.write(s)
+        
+    if st.checkbox('Check NULL values count'):
+        st.write(data.isnull().sum())
+        
+    if st.checkbox('Check Unique Value count'):
+        st.write(data.nunique())
+        
+    if st.checkbox('Check datatypes'):
+        st.write(data.dtypes)
+        
     if st.checkbox('Explore all data \t \t  (Click to expand)\n'):
         st.write(data)
-    if st.checkbox('View dataset dimnesion'):
-        st.write(data.shape)
-    if st.checkbox('View columns'):
-        columns = data.columns.to_list()
+        
+    # if st.checkbox('Check dataset dimension'):
+    #     st.write(data.shape)
+ 
+    if st.checkbox("Show Selected Columns"):
+        selected_columns = st.multiselect("Select Columns",columns)
+        new_df = data[selected_columns]
+        st.dataframe(new_df)
+    if st.checkbox('Correlation Matrix'):
+        st.write(data.corr())
+        ax = sns.heatmap(data.corr(),annot=True)
+        st.pyplot()
+
+    # if st.checkbox('Graphs'):
+    #     gtype = st.selectbox('Select option', ['No Selection','bar', 'line', 'histogram'],index = 0)
+    #     if gtype == 'bar':
+    #         selX=st.selectbox('Select X axis column', columns)
+    #         selY=st.selectbox('Select Y axis column', columns)
+    #         Xdf = data[selX]
+    #         Ydf = data[selY]
+            
+
+            #st.write(chartDF.head())
+        #     st.altair_chart(chartDF)
+        # if gtype == 'line':
+        #     selcol = st.multiselect("Select Columns",columns)
+        #     st.pyplot(plt.hist(data[selcol]))
+            
+        #     # st.pyplot(fig)
+        #     st.write(selcol[0])
+            
+        # if gtype == 'histogram':
+        #     selcol=st.radio('Select column', columns)
+        #     chart= data[columns].plot(kind='hist')
+        #     st.pyplot(chart)
+		
+		
+    
+        
+    if st.checkbox('Examine column names'):
         st.write(columns)
-    if st.checkbox('View numerical data statistics'):
+        
+    if st.checkbox('Examine numerical data statistics'):
         st.write(data.describe())
-    if st.checkbox('View non-numerical data statistics'):
-        if data.describe(include=['object', 'bool']):
+        
+    if st.checkbox('Examine non-numerical data statistics'): #need to handle when no non numeric data exists
+        val = data.describe(include=['object', 'bool'])
+        if val is not None:
             st.write(data.describe(include=['object', 'bool']))
         else:
             st.write('No non-numerical data in the dataset')
+    
     
 if __name__ == '__main__':
 	main()
